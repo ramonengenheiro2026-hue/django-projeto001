ac3219cf50c3a9173f8572f51eff038de3e6b95c
Ramon Algaranhar Pereira <ramon.engenheiro2026@gmail.com>
2026-09-22 12:14:16 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name' : 'Ramon Algaranhar Pereira',
     })
-
-
-def sobre(request):
-    return HttpResponse('SOBRE RECIPES')
-
-def contato(request):
-    return HttpResponse('CONTATO RECIPES')
